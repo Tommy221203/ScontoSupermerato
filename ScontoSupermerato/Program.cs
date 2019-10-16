80959352aa3b63750db276270903fb890bffefaf
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:08:35 +0200

    finito

--- ScontoSupermerato/Program.cs
+++ ScontoSupermerato/Program.cs
@@ -6,7 +6,19 @@ namespace ScontoSupermerato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Quale è stato l'importo della spesa?");
+            double spesa = double.Parse(Console.ReadLine());
+            if (spesa >= 100)
+            {
+                double percentuale = (spesa * 20) / 100;
+                double finale = spesa - percentuale;
+                Console.WriteLine($"L'importo finale è {finale:c}");
+            }
+            else
+            {
+                Console.WriteLine("la spesa non supera i 100 euro");
+            }
+            Console.ReadLine();
         }
     }
 }
